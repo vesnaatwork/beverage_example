d3c6b9202caec7f435f6ca99eaf979fed6797328
vesnatwork <vesna@work.co>
2020-03-29 19:14:39 +0200

    Add pubsub for object creation

--- src/Presenter.js
+++ src/Presenter.js
@@ -1,47 +1,19 @@
-//samo prezentovanje ispisivanje stvari sta se desilo
-//informativne poruke kreiran su objekti, nakon akcije
-//greske
-//warning
-//success
+class Presenter{
+    constructor(pubsub){
+        pubsub.subscribe("user", this.onUser.bind(this));
+        pubsub.subscribe("glass", this.onGlass.bind(this));
+        pubsub.subscribe("beverage", this.onBeverage.bind(this));
 
-
-//pub sub pattern
-//samo formatira poruke za izlaz i komunicira sa consolom
-// function pubSub() {
-//     const subscribers = {}
-//     function publish(eventName, data) {
-//         if (!Array.isArray(subscribers[eventName])) {
-//           return
-//         }
-//         subscribers[eventName].forEach((callback) => {
-//           callback(data)
-//         })
-//       }
-    
-//     function subscribe(eventName, callback) {
-//         if (!Array.isArray(subscribers[eventName])) {
-//           subscribers[eventName] = []
-//         }
-//         subscribers[eventName].push(callback)
-//       }
-    
-//       function subscribe(eventName, callback) {
-//     if (!Array.isArray(subscribers[eventName])) {
-//       subscribers[eventName] = []
-//     }
-//     subscribers[eventName].push(callback)
-//   }
-
-//   function showMeTheMoney(money) {
-//     console.log(money)
-//   }
-  
-//   const ps = pubSub()
-//   ps.publish('show-money', 1000000)
-//   ps.subscribe('show-money', showMeTheMoney)
-//       return {
-//         subscribe,
-//         publish
-//       }  
-
+    }
+    onUser(data){
+        console.log("User "+data.name);
+    }
+    onGlass(data){
+        console.log("Glass volume= "+data.maxvolume+" name ="+ data.name)
+    }
+    onBeverage(data){
+        console.log("Beverage "+data.name+" type= "+data.type+" volume= "+data.volume);
+    }
 }
+module.exports = Presenter;
+//inerpolacija za consoloe log
--- src/model/Glass.js
+++ src/model/Glass.js
@@ -1,28 +1,33 @@
 
 class Glass
 {
-    constructor( maxvolume, name)
+    constructor( maxvolume, name, pubsub)
     {   
-        if (maxvolume == null)
-        throw "400"
-        if (name==null)
-        throw "400"
-        this.maxvolume=maxvolume
-        this.name=name;
-        this.volume=0;
+        if (maxvolume == null){
+            throw new ValidationError ("Glass maximum volume can't be null")
+        }
+        if (name == null)
+        {
+            throw new ValidationError ("Glass name can't be null")
+        }
+        this.maxvolume = maxvolume
+        this.name = name;
+        this.volume = 0;
+        pubsub.publish("glass", {name:this.name, maxvolume:this.maxvolume})
     }
   
     fill(beverage){
-       if(this.volume+beverage.volume>this.maxvolume)
-       throw "400"
-        this.volume+=beverage.volume;
-        this.beverage=beverage
+       if(this.volume+beverage.volume>this.maxvolume){
+        new ValidationError (str.concat ("Volume can't be larger than ",this.maxvolume))
+       }
+        this.volume += beverage.volume;
+        this.beverage = beverage
     }
 
     spill(beverage){
         if (this.volume-beverage.volume<0)
-            throw "400"
-        this.volume-=beverage.volume;  
+        new ValidationError (str.concat("Volume can't be less than ",this.volume))
+        this.volume -= beverage.volume;  
     }
 
 }
