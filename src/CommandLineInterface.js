cd83fe5be5a8e0ef776c858ef15292e0605675f4
vesnatwork <vesna@work.co>
2020-04-22 19:59:00 +0200

    Add command line but with all questions

--- src/CommandLineInterface.js
+++ src/CommandLineInterface.js
@@ -4,57 +4,167 @@ class CommandLineInterface {
     constructor(){
         console.log("Welcome to this app");
     }
-  askUser() {
-    const userQuestions = [
-      {
-        name: "Name",
-        type: "input",
-        message: "Enter your name:",
-        validate: function(value) {
-          if (value.length) {
-            return true;
-          } else {
-            return "Please enter your name.";
+   
+    async askAllQuestions(){
+      const allQuestions = [
+        {
+          name: "name",
+          type: "input",
+          message: "Enter your name:",
+          validate: function(value) {
+            if (value.length) {
+              return true;
+            } else {
+              return "Please enter your name.";
+            }
           }
+        },
+        {
+          name: "beverageName",
+          type: "input",
+          message: "Enter your beverage name:",
+          validate: function(value) {
+            if (value.length) {
+              return true;
+            } else {
+              return "Please enter your beverage name.";
+            }
+          }
+        },
+        {
+          name: "type",
+          type: "input",
+          message: "Enter your beverage type:",
+          validate: function(value) {
+            if (value.length) {
+              return true;
+            } else {
+              return "Please enter your beverage type.";
+            }
+          }
+        },
+        {
+          name: "volume",
+          type: "number",
+          message: "Enter your beverage volume:",
+          validate: function(value) {
+            var valid = !isNaN(parseFloat(value));
+            return valid || 'Please enter a number';
+          },
+          filter: Number
+        },
+        {
+          name: "glassName",
+          type: "input",
+          message: "Describe your glass:",
+          validate: function(value) {
+            if (value.length) {
+              return true;
+            } else {
+              return "Describe your glass:";
+            }
+          }
+        },
+        {
+          name: "maxvolume",
+          type: "number",
+          message: "How much fits in your glass in ml:",
+          validate: function(value) {
+            var valid = !isNaN(parseFloat(value));
+            return valid || 'Please enter a number';
+          },
+          filter: Number
         }
-      }
-    ];
-    return inquirer.prompt(userQuestions);
-  }
 
-  askBeverage() {
-    const beverageQuestions = [
-      {
-        name: "Beverage",
-        type: "input",
-        message: "Enter your beverage:",
-        validate: function(value) {
-          if (value.length) {
-            return true;
-          } else {
-            return "Please enter your beverage.";
-          }
-        }
-      }
-    ];
-    return inquirer.prompt(beverageQuestions);
+      ];
+  return inquirer.prompt(allQuestions)
+    }
+   
+    async askUser() {
+      const UserQuestions = [
+              {
+                name: "name",
+                type: "input",
+                message: "Enter your name:",
+                validate: function(value) {
+                  if (value.length) {
+                    return true;
+                  } else {
+                    return "Please enter your name.";
+                  }
+                }
+              }
+            ];
+        return inquirer.prompt(UserQuestions)
+      
   }
-  askGlass() {
-    const beverageQuestions = [
-      {
-        name: "Glass",
-        type: "input",
-        message: "Describe your glass:",
-        validate: function(value) {
-          if (value.length) {
-            return true;
-          } else {
-            return "Please enter your beverage.";
+  
+    async askBeverage() {
+      const beverageQuestions = [
+        {
+          name: "name",
+          type: "input",
+          message: "Enter your beverage name:",
+          validate: function(value) {
+            if (value.length) {
+              return true;
+            } else {
+              return "Please enter your beverage name.";
+            }
           }
+        },
+        {
+          name: "type",
+          type: "input",
+          message: "Enter your beverage type:",
+          validate: function(value) {
+            if (value.length) {
+              return true;
+            } else {
+              return "Please enter your beverage type.";
+            }
+          }
+        },
+        {
+          name: "volume",
+          type: "number",
+          message: "Enter your beverage volume:",
+          validate: function(value) {
+            var valid = !isNaN(parseFloat(value));
+            return valid || 'Please enter a number';
+          },
+          filter: Number
         }
-      }
-    ];
-    return inquirer.prompt(beverageQuestions);
+      ];
+      return inquirer.prompt(beverageQuestions);
+    }
+    async askGlass() {
+      const GlassQuestions = [
+        {
+          name: "name",
+          type: "input",
+          message: "Describe your glass:",
+          validate: function(value) {
+            if (value.length) {
+              return true;
+            } else {
+              return "Describe your glass:";
+            }
+          }
+        },
+        {
+          name: "maxvolume",
+          type: "number",
+          message: "How much fits in your glass in ml:",
+          validate: function(value) {
+            var valid = !isNaN(parseFloat(value));
+            return valid || 'Please enter a number';
+          },
+          filter: Number
+        }
+      ];
+      return inquirer.prompt(beverageQuestions);
+    }
   }
-}
-module.exports = CommandLineInterface;
+  
+ module.exports = CommandLineInterface;
--- src/Logger.js
+++ src/Logger.js
@@ -6,17 +6,17 @@ class Logger{
         
         fs.open(this.filename, 'w', function (err, file) {
             if (err) throw err;
-            console.log('Saved!');
           }); 
     }
 
     onEvent(data){
         const date = new Date();
-        fs.appendFile(this.filename,`${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}-${date.getHours()}-${date.getMinutes()} ${data.toString()}`, function (err) {
+        fs.appendFile(this.filename,`${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()} ${date.getHours()}-${date.getMinutes()} ${data.toString()}`, function (err) {
             if (err) throw err;
-            console.log('Saved!');
           })
     }
    
 }
 module.exports = Logger;
+
+//izvuci date objekat
